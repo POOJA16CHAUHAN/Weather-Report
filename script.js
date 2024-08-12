ef521f380d1213377a8c91255e1e8cc5b4b1794d
puja chauhan <pc8075448@gmail.com>
2024-08-12 10:50:38 +0530

    added channel name

--- script.js
+++ script.js
@@ -10,76 +10,12 @@ const weather_body = document.querySelector('.weather-body');
 
 
  async function checkWeather(city){
-    // const api_key = "024fda41f8e25b0ffdb026381989d425";
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
-
-    //const api_key = "024fda41f8e25b0ffdb026381989d425";
     const api_key = "bb522d080e0467215ffee9a67a46c1a4";
-    //  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
-    // const weather_data = await fetch(`${url}`).then
-    // (response => response.json());
-
-
-    // if(weather_data.cod === 404){
-    //     location_not_found.style.display = "flex";
-    //     weather_body.style.display = "none";
-    //     console.log("error");
-    //     return;
-    // }
-
-    // console.log("run");
-    // location_not_found.style.display = "none";
-    // weather_body.style.display = "flex";
-    // temperature.innerHTML = `${Math.round
-    //     (weather_data.main.temp - 273.15)}°C`;
-    // description.innerHTML = `${weather_data.weather[0].
-    //     description}`;
-
-    // humidity.innerHTML = `${weather_data.main.humidity}
-    // %`;
-    // wind_speed.innerHTML = `${weather_data.wind.speed}
-    // Km/H`;
-
-
-    // switch(weather_data.weather[0].main){
-    //     case 'Clouds':
-    //         weather_img.src = "clooud.png";
-    //         weather_img.src = ".clooud.png";
-    //         weather_img.src = "./cloud.png";
-    //         weather_img.src = "../clooud.png";
-    //         break;
-    //     case 'Clear':
-    //         weather_img.src = "sun3.png";
-    //         weather_img.src = ".sun3.png";
-    //         weather_img.src = "./sun3.png";
-    //         weather_img.src = "../sun3.png";
-    //         break;
-    //      case 'Rain':
-    //             weather_img.src = "sun4.png";
-    //             weather_img.src = "/sun4.png";
-    //             weather_img.src = "./sun4.png";
-    //             weather_img.src = "../sun4.png";
-    //             break;
-    //     case 'Mist':
-    //             weather_img.src = "sunn.png";
-    //             weather_img.src = "/sunn.png";
-    //             weather_img.src = "./sunn.png";
-    //             weather_img.src = "../sunn.png";
-    //             break;
-        
     
-    //     }
-    
-    //     console.log(weather_data);
-    // }
-    
-    
-    // searchBtn.addEventListener('click', ()=>{
-    //     checkWeather(inputBox.value);
-    // });
 
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
@@ -104,34 +40,29 @@ const weather_body = document.querySelector('.weather-body');
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            // weather_img.src = "images/cloud.png";
-            // weather_img.src = "/images/cloud.png";
+            
             weather_img.src = "./images/cloud.png";
-            // weather_img.src = "../images/cloud.png";
+            
             break;
         case 'Clear':
-            // weather_img.src = "images/clear.png";
-            // weather_img.src = "/images/clear.png";
+            
             weather_img.src = "./images/clear.png";
-            // weather_img.src = "../images/clear.png";
+            
             break;
         case 'Rain':
-            // weather_img.src = "images/rain.png";
-            // weather_img.src = "/images/rain.png";
+           
             weather_img.src = "./images/rain.png";
-            // weather_img.src = "../images/rain.png";
+            
             break;
         case 'Mist':
-            // weather_img.src = "images/mist.png";
-            // weather_img.src = "/images/mist.png";
+            
             weather_img.src = "./images/mist.png";
-            // weather_img.src = "../images/mist.png";
+            
             break;
         case 'Snow':
-            // weather_img.src = "images/snow.png";
-            // weather_img.src = "/images/snow.png";
+            
             weather_img.src = "./images/snow.png";
-            // weather_img.src = "../images/snow.png";
+            
             break;
 
     }
